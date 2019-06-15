4fa0945e68dd92c01cdfef6be545377e9aa7a7ea
John McAllister <jmcall10@uncc.edu>
2019-06-15 03:34:19 -0400

    image grow test

--- imagegrow.js
+++ imagegrow.js
@@ -1,11 +1,9 @@
 function imageEnlarge() {
  document.getElementById('1').style.height="10%";
  document.getElementById('1').style.width="10%";
- document.getElementById('1').style.position='absolute';
  }
 function imageReset() {
 
  document.getElementById('1').style.height="80%";
  document.getElementById('1').style.width="80%";
- document.getElementById('1').style.position='absolute';
- }
+}
